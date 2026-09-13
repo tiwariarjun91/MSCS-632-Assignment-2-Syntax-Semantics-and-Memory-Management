e2d1444bc7db41020a9501146c09246b7d0c4a2d
Arjun Tiwari <tiwariarjun91@gmail.com>
2026-09-13 11:14:05 -0700

    Javascript run for section 2
    Signed-off-by: Arjun Tiwari <tiwariarjun91@gmail.com>

---
+++ Section 2/javaScript_section_2.js
@@ -0,0 +1,14 @@
+let price = 12.50;
+let quantity = 3;
+
+let total = price * quantity;
+console.log("Order total:", total);
+
+price = "12.50";
+
+console.log("Price type:", typeof price);
+
+let result = price + quantity;
+
+console.log("Mixed result:", result);
+console.log("Result type:", typeof result);
